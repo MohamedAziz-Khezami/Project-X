5ec62c4318a6c9dcf7575570120dfc9e3d803ddc
Mohamed Aziz Khezami <khezamimohamedaziz@tbs.u-tunis.tn>
2024-07-14 11:29:28 +0100

    change

--- app.py
+++ app.py
@@ -1,1 +1,57 @@
-#hello
+
+import streamlit as st 
+import altair as alt
+import datetime as dt
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+
+sp500 =yf.download("^GSPC", start="2023-01-01", end="2024-07-12")
+
+sp500 = sp500.reset_index()
+
+sp = sp500[['Date','Close']]
+
+st.set_page_config(
+    layout="wide",
+    page_title="Sentimental Stocks",
+    page_icon="📈",
+    )
+
+ 
+def main():
+    
+  
+
+    
+    
+    predictions= pd.read_csv("/Users/mak/Desktop/Code_With_Me/Sentiment project/predictions.csv")
+    
+
+    
+    
+    chart1 = alt.Chart(predictions).mark_circle(color='blue').encode(
+    x=alt.X('ds', axis=alt.Axis(title='Date'), type='temporal'),
+    y=alt.Y('yhat', axis=alt.Axis(title='Predicted Price')),
+    ).interactive()
+
+# Define the second chart
+    chart2 = alt.Chart(sp).mark_line(color='red').encode(
+    x=alt.X('Date', axis=alt.Axis(title='Date'), type='temporal'),
+    y=alt.Y('Close', axis=alt.Axis(title='Closing Price')),
+    ).interactive()
+    
+    # Layer the charts
+    combined_chart = alt.layer(chart1, chart2)
+
+# Display the chart in Streamlit
+    st.altair_chart(combined_chart, theme=None, use_container_width=True)
+    
+    
+
+    
+    
+
+if __name__ == "__main__":
+    main()
---
+++ dags/dag2.py
@@ -0,0 +1,129 @@
+import logging
+import pendulum
+import requests
+from airflow.decorators import dag, task, task_group
+from airflow.models import Variable
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+import duckdb
+import pandas as pd
+from newsdataapi import NewsDataApiClient
+import datetime as dt
+from pysentimiento import create_analyzer
+from prophet import Prophet
+import csv
+
+
+
+sentiments = create_analyzer(task="sentiment", lang="en")
+emotions = create_analyzer(task='emotion', lang="en")
+
+apikey = "pub_484350d0460b3a6d9a45319e3f2fcfe2f8dc3"
+
+
+
+@dag('pleaserun',start_date=dt.datetime.now(), schedule_interval='*/7 * * * *')
+def dag1():
+
+    @task
+    def get_news():
+        try:
+            
+            api = NewsDataApiClient(apikey=apikey)
+
+
+            response = api.news_api( q= 's&p500' ,language="en")
+            
+            articles = pd.DataFrame(response['results'])
+            articles = articles[['pubDate', 'title']]
+            articles.pubDate = pd.to_datetime(articles.pubDate)
+            articles.pubDate = articles.pubDate.apply(lambda x : x.strftime('%Y-%m-%d'))
+            articles.pubDate = articles.pubDate.astype(object)
+            articles = articles.groupby('pubDate')['title'].sum()
+            articles = pd.DataFrame(articles)
+            articles = articles.reset_index()
+            articles = articles[articles['pubDate'] == str(dt.date.today() - dt.timedelta(days = 1))]
+            articles.loc[:,'pubDate'] = str(dt.date.today())
+            articles = articles.rename(columns = {'pubDate' : 'ds'})
+            return articles
+
+
+        except Exception as e:
+            print(e)
+            return None
+        
+        
+    @task
+    def get_sentiments(articles):
+        
+        articles['negative'] = articles['title'].apply(lambda x : sentiments.predict(x).probas['NEG'])
+        articles['positive'] = articles['title'].apply(lambda x : sentiments.predict(x).probas['POS'])
+        articles['neutral'] = articles['title'].apply(lambda x : sentiments.predict(x).probas['NEU'])
+        articles['anger'] = articles['title'].apply(lambda x: emotions.predict(x).probas['anger'])
+        articles['disgust'] = articles['title'].apply(lambda x:emotions.predict(x).probas['disgust'])
+        articles['fear'] = articles['title'].apply(lambda x:emotions.predict(x).probas['fear'])
+        articles['joy'] = articles['title'].apply(lambda x:emotions.predict(x).probas['joy'])
+        articles['sadness'] = articles['title'].apply(lambda x:emotions.predict(x).probas['sadness'])
+        articles['surprise'] = articles['title'].apply(lambda x:emotions.predict(x).probas['surprise'])
+        articles['others'] = articles['title'].apply(lambda x:emotions.predict(x).probas['others'])
+        
+    
+        
+        articles = articles[['ds', 'negative', 'positive', 'neutral', 'anger', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'others']]
+        
+        
+        return articles
+            
+
+    @task
+    def fit_prediction(articles):
+        fit_data = pd.read_csv('/Users/mak/Desktop/Code_With_Me/Sentiment project/Dataset.csv')
+        fit_data = fit_data.rename(columns = {'Date': 'ds', 'Close':'y'})
+        p = Prophet()
+        p.add_regressor('negative')
+        p.add_regressor('positive')
+        p.add_regressor('neutral')
+        p.add_regressor('anger')
+        p.add_regressor('disgust')
+        p.add_regressor('fear')
+        p.add_regressor('joy')
+        p.add_regressor('sadness')
+        p.add_regressor('surprise')
+        p.add_regressor('others')
+        p.add_country_holidays(country_name='US')
+        
+        p.fit(fit_data)
+        
+        prediction = p.predict(articles)
+        
+        prediction = prediction[['ds','yhat']]
+        
+        prediction.ds = prediction.ds.apply(lambda x : x.strftime('%Y-%m-%d'))
+        prediction.ds = prediction.ds.astype(object)
+        row = list(prediction.iloc[0])
+        
+        
+        
+        with open('/Users/mak/Desktop/Code_With_Me/Sentiment project/predictions.csv', 'a', newline='') as csvfile:
+            # Create a CSV writer object
+            writer = csv.writer(csvfile)
+            # Write the new row data
+            writer.writerow(row)
+
+    
+
+    articles = get_news()
+
+    articles_sentiments = get_sentiments(articles)
+
+    fit_prediction(articles_sentiments)
+    
+    
+    
+
+dag1()
+        
+        
+        
+            
+            
